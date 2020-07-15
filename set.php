c3012543fb383174719d81486fc57d239731c39c
MaryaKharya <korcemkinamatia@gmail.com>
2020-07-15 10:10:58 +0300

    now

--- set.php
+++ set.php
@@ -1,22 +1,28 @@
 <?php
-// Load composer
-require __DIR__ . '/vendor/autoload.php';
 
-$bot_api_key = 'your:bot_api_key';
+// Load composer
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Add you bot's API key and name
+$bot_api_key  = 'your:bot_api_key';
 $bot_username = 'username_bot';
-$result = $telegram -> setWebhook($hook_url, ['certificate' => '/path/to/certificate']);
-$hook_url = 'https://your-domain/path/to/hook.php';
+$result = $telegram->setWebhook($hook_url, ['certificate' => '/path/to/certificate']);
+// Define the URL to your hook.php file
+$hook_url     = 'https://your-domain/path/to/hook.php';
 
 try {
- // Create Telegram API object
+    // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
- // Set webhook
-    $result = $telegram -> setWebhook($hook_url);
- if($result -> isOk()) {
- echo $result -> getDescription();
- }
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+
+    // To use a self-signed certificate, use this line instead
+    //$result = $telegram->setWebhook($hook_url, ['certificate' => $certificate_path]);
+
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
- // log telegram errors
-    // echo $e->getMessage();
+    echo $e->getMessage();
 }
