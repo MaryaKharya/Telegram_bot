eaf1ec7f73fee5e1196513e62a2640aa0879fda9
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-15 14:19:50 +0300

    new

---
+++ installhandler.php
@@ -0,0 +1,27 @@
+<?php
+    function regHandler($cert, $token, $murl) 
+    {
+        $url = "https://api.telegram.org/bot" . $token . "/setWebhook";
+        $ch = curl_init();
+        $optArray = array(
+                CURLOPT_URL => $url,
+                CURLOPT_POST => true,
+                CURLOPT_SAFE_UPLOAD => false,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_POSTFIELDS => array('url' => $murl, 'certificate' => '@' . realpath($cert))
+        );
+        curl_setopt_array($ch, $optArray);
+        
+        $result = curl_exec($ch);
+        echo "<pre>";
+            print_r($result);
+        echo "</pre>";
+        curl_close($ch);
+    }
+
+    $token = '794519976:AAFVA4NguNYVsSymwPqn0iVHrBVoDIeMNnE';
+    $path = '/ssl/YOURPUBLIC.pem';
+    $handlerurl = 'https://maryaharya.ru/Telegram_bot/installhandler.php'; // ИЗМЕНИТЕ ССЫЛКУ
+    
+    regHandler($path, $token, $handlerurl);
+?>
