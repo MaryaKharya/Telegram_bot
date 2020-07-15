044e515ecf8b0b9d1a39593edab8021f4c0875da
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-15 14:38:30 +0300

    new

---
+++ handler.php
@@ -0,0 +1,17 @@
+ <?php
+ function sendMessage($chat_id, $message) 
+ {
+ file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+ }
+ 
+ $access_token = 'YOUR TOKEN';
+ $api = 'https://api.telegram.org/bot' . $access_token;
+ 
+ 
+ $output = json_decode(file_get_contents('php://input'), TRUE);
+ $chat_id = $output['message']['chat']['id'];
+ $first_name = $output['message']['chat']['first_name'];
+ $message = $output['message']['text'];
+ 
+ $preload_text = $first_name . ', я получила ваше сообщение!';
+ sendMessage($chat_id, $preload_text);
