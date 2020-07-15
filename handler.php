dc74692cb6c97994ba6a40003281fe7e3d67e253
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-15 21:53:38 +0300

    marya

--- handler.php
+++ handler.php
@@ -1,38 +1,35 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
+ <?php
+ function sendMessage($chat_id, $message) 
+ {
+ file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+ }
  
-require_once("vendor/autoload.php");
-
-$access_token = '794519976:AAFVA4NguNYVsSymwPqn0iVHrBVoDIeMNnE';
-$bot = new \TelegramBot\Api\Client($token);
-
-
-// если бот еще не зарегистрирован - регистрируем
-if(!file_exists("registered.trigger")){ 
-	/**
-	 * файл registered.trigger будет создаваться после регистрации бота. 
-	 * если этого файла нет значит бот не зарегистрирован 
-	 */
-	 
-	// URl текущей страницы
-	$page_url = "https://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-	$result = $bot->setWebhook($page_url);
-	if($result){
-		file_put_contents("registered.trigger",time()); // создаем файл дабы прекратить повторные регистрации
+ $access_token = '794519976:AAFVA4NguNYVsSymwPqn0iVHrBVoDIeMNnE';
+ $api = 'https://api.telegram.org/bot' . $access_token;
+ 
+ 
+ $output = json_decode(file_get_contents('php://input'), TRUE);
+ $chat_id = $output['message']['chat']['id'];
+ $first_name = $output['message']['chat']['first_name'];
+ $message = $output['message']['text'];
+ 
+if ($message)
+{
+    if ($message == "/start")
+    {
+        $preload_text = $first_name . ', добро пожаловать!';
+    }
+	elseif ($message == "привет")
+	{
+		$preload_text = $first_name . ', приветик!';
+	}
+	elseif ($message == "пока")
+	{
+		$preload_text = $first_name . ', пока, пока!';
+	}
+	else
+	{
+		$preload_text = $first_name . ', мой создатель программист херов, на больше команд он пока не способен, так что... соре!';
 	}
 }
-// обязательное. Запуск бота
-$bot->command('start', function ($message) use ($bot) {
-    $answer = 'Добро пожаловать!';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
-
-// помощ
-$bot->command('help', function ($message) use ($bot) {
-    $answer = 'Команды:
-/help - помощ';
-    $bot->sendMessage($message->getChat()->getId(), $answer);
-});
-
-// запускаем обработку
-$bot->run();
+ sendMessage($chat_id, $preload_text);
