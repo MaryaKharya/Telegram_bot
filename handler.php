9dbdc2d524cc0b1139fcda4455f160d78ac37c48
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-16 12:21:21 +0300

    marya

--- handler.php
+++ handler.php
@@ -7,8 +7,12 @@ require_once("vendor/autoload.php");
 $token = "794519976:AAFVA4NguNYVsSymwPqn0iVHrBVoDIeMNnE";
 $bot = new \TelegramBot\Api\Client($token);
 // если Телеграм-бот не зарегистрирован - регистрируем
-
 // обязательное. Запуск бота
+
+if ($message == "Гифка") {
+            $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
+            $bot->sendDocument($message->getChat()->getId(), $url);
+}
 $bot->command('start', function ($message) use ($bot) {
 $answer = 'Добро пожаловать!';
 $bot->sendMessage($message->getChat()->getId(), $answer);
@@ -23,6 +27,8 @@ $bot->sendMessage($message->getChat()->getId(), $answer);
     $bot->command('ping', function ($message) use ($bot) {
         $bot->sendMessage($message->getChat()->getId(), 'pong!');
     });
+
+if ($message == )
 
 // передаем картинку
 $bot->command('getpic', function ($message) use ($bot) {
