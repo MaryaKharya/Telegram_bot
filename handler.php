e5360978b9025c47dee377b9beb4bcc88c9dc6d4
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-16 19:40:27 +0300

    marya

--- handler.php
+++ handler.php
@@ -103,7 +103,7 @@ if (!empty($data['message']['text'])) {
 			'sendPhoto', 
 			array(
 				'chat_id' => $data['message']['chat']['id'],
-				'photo' => https://sun9-3.userapi.com/c10056/u81896685/-6/y_ba15737f.jpg
+				'photo' => "https://sun9-3.userapi.com/c10056/u81896685/-6/y_ba15737f.jpg"
 			)
 		);
 		
