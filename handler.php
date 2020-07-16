31874cd5343964f04d197fe66c16ace0b58e0777
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-16 11:32:23 +0300

    marya

--- handler.php
+++ handler.php
@@ -5,6 +5,16 @@ use \TelegramBot\Api;
 
 $bot = new \TelegramBot\Api\BotApi('794519976:AAFVA4NguNYVsSymwPqn0iVHrBVoDIeMNnE');
 
-$bot->sendMessage($chatId, $messageText);
+$bot->command('start', function ($message) use ($bot) {
+    $answer = 'Добро пожаловать!';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
+
+// помощ
+$bot->command('help', function ($message) use ($bot) {
+    $answer = 'Команды:
+/help - помощ';
+    $bot->sendMessage($message->getChat()->getId(), $answer);
+});
 
 $bot->run();
