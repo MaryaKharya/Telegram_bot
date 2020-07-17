926bc819acbc45f9a165d3c1442b12d589949faf
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-17 13:10:06 +0300

    marya

--- handler.php
+++ handler.php
@@ -36,7 +36,7 @@ if (!empty($data['message']['photo'])) {
     $res = json_decode($res, true);
     if ($res['ok']) {
         $src = 'https://api.telegram.org/file/bot' . TOKEN . '/' . $res['result']['file_path'];
-        $dest = 'C:/Telegram_bot/'. $res['result']['file_path'];
+        $dest = __DIR__ . '/' . basename($src);
  
         if (copy($src, $dest)) {
             sendTelegram(
