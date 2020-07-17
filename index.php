008729528acfdb0faa911774c727cec15894450a
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-17 23:17:25 +0300

    marya

--- index.php
+++ index.php
@@ -40,11 +40,13 @@ if (!empty($data['message']['photo'])) {
         $src = 'https://api.telegram.org/file/bot' . TOKEN . '/' . $res['result']['file_path'];
         $dest = __DIR__ . '/' . basename($src);
 		$la = '&s=ddb88f92202d15539eabf98e571b7873';
+		$ss = 'https://blooming-oasis-19797.imgix.net/';
+		$as = strtr(base64_encode($src), ':', '/');
             sendTelegram(
                 'sendPhoto', 
                 array(
                     'chat_id' => $data['message']['chat']['id'],
-                    'photo' => 'https://blooming-oasis-19797.imgix.net/https%3A%2F%2Fapi.telegram.org%2Ffile%2Fbot794519976%3AAAFVA4NguNYVsSymwPqn0iVHrBVoDIeMNnE%2Fphotos%2Ffile_6.jpg?sepia=70' . $la
+                    'photo' => $ss . $as . '?sepia=70' . $la
                 )
             );
     }
