e3a7c2da997a540ea1ae0ba07fb0da4d2f8e31f3
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-17 14:58:27 +0300

    marya

--- handler.php
+++ handler.php
@@ -43,7 +43,7 @@ if (!empty($data['message']['photo'])) {
                 'sendMessage', 
                 array(
                     'chat_id' => $data['message']['chat']['id'],
-                    'text' => 'Фото сохранено'
+                    'photo' => curl_file_create($dest)
                 )
             );
             
