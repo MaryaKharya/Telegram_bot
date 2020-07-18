42e2832b042c3da2a3044240d92031a5345f5228
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-18 23:10:13 +0300

    marya

--- index.php
+++ index.php
@@ -39,22 +39,29 @@ if (!empty($data['message']['photo'])) {
     if ($res['ok']) {
         $src = 'https://api.telegram.org/file/bot' . TOKEN . '/' . $res['result']['file_path'];
 				$key = 'e592f995c2f3ae18d817f61aff1764b2';
-$client = new \GuzzleHttp\Client();
-$response = $client->request('POST', 'http://api.convertio.co/convert', [
-        'apikey' => 'e592f995c2f3ae18d817f61aff1764b2',
-        'input' => 'url',
-        'file' => 'https://sun1-15.userapi.com/vy0zsJaIsMMTh7nwTkkDBA1VpRzfL7ehwPRm_A/mBXzn2D0j5Q.jpg',
-		'filename' => '',
-        'outputformat' => 'png',
-		'options' => ''
-]);
 
+$url = 'http://api.convertio.co/convert';
+$data = array('apikey' => 'e592f995c2f3ae18d817f61aff1764b2', 'input' => 'url', 'file' => 'https://sun1-15.userapi.com/vy0zsJaIsMMTh7nwTkkDBA1VpRzfL7ehwPRm_A/mBXzn2D0j5Q.jpg', 'outputformat' => 'png',);
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
 return $response->getBody();
             sendTelegram(
                 'sendMessage', 
                 array(
                     'chat_id' => $data['message']['chat']['id'],
-                    'text' => $response->getBody()
+                    'text' => $result
                 )
             );
     }
