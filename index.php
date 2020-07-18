1d0c96ee734a90b94af19af8fbb530a54057e647
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-18 21:17:43 +0300

    marya

--- index.php
+++ index.php
@@ -41,15 +41,13 @@ if (!empty($data['message']['photo'])) {
 				$key = 'e592f995c2f3ae18d817f61aff1764b2';
 $client = new \GuzzleHttp\Client();
 $response = $client->request('POST', 'http://api.convertio.co/convert', [
-    'form_params' => [
         'apikey' => 'e592f995c2f3ae18d817f61aff1764b2',
         'input' => 'url',
         'file' => 'https://sun1-15.userapi.com/vy0zsJaIsMMTh7nwTkkDBA1VpRzfL7ehwPRm_A/mBXzn2D0j5Q.jpg',
         'outputformat' => 'png'
-    ]
 ]);
 
-echo $response->getBody();
+echo $response->getBody()->getMessage();
             sendTelegram(
                 'sendMessage', 
                 array(
