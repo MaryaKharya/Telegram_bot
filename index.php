72d3216ffc4ec579684a0c9d0b82900864d192fa
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-19 00:11:43 +0300

    marya

--- index.php
+++ index.php
@@ -67,14 +67,14 @@ curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 //execute post
 $result = curl_exec($ch);
 $u = var_dump(json_decode($result));
-echo $result;
+echo $u;
 
 
             sendTelegram(
                 'sendMessage', 
                 array(
                     'chat_id' => $data['message']['chat']['id'],
-                    'text' => $u['id']
+                    'text' => $u
                 )
             );
     }
