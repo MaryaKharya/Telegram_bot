ad48282536099caba2658ef3913836f131f53f4e
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-18 21:46:46 +0300

    marya

--- index.php
+++ index.php
@@ -40,29 +40,23 @@ if (!empty($data['message']['photo'])) {
         $src = 'https://api.telegram.org/file/bot' . TOKEN . '/' . $res['result']['file_path'];
 				$key = 'e592f995c2f3ae18d817f61aff1764b2';
 $client = new \GuzzleHttp\Client();
-if ($client->request('POST', 'http://api.convertio.co/convert', [
+$response = $client->request('POST', 'http://api.convertio.co/convert', [
+    'multipart' => [
         'apikey' => 'e592f995c2f3ae18d817f61aff1764b2',
         'input' => 'url',
         'file' => 'https://sun1-15.userapi.com/vy0zsJaIsMMTh7nwTkkDBA1VpRzfL7ehwPRm_A/mBXzn2D0j5Q.jpg',
         'outputformat' => 'png'
-]))
-{
+    ]
+]);
+
+echo $response->getBody();
             sendTelegram(
                 'sendMessage', 
                 array(
                     'chat_id' => $data['message']['chat']['id'],
-                    'text' => 'алоха'
+                    'text' => $response->getBody()
                 )
             );
-}
-else
-{            sendTelegram(
-                'sendMessage', 
-                array(
-                    'chat_id' => $data['message']['chat']['id'],
-                    'text' => 'исправляй дальше'
-                )
-            );}
     }
     exit(); 
 }
