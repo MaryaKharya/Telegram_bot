1c5578aff0c7d5bfda35cc54e5b5cea618be2bc1
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-18 18:01:21 +0300

    marya

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ foreach($lo as $item) {
                 'sendMessage', 
                 array(
                     'chat_id' => $data['message']['chat']['id'],
-                    'text' => $sa
+                    'text' => $result
                 )
             );
     }
