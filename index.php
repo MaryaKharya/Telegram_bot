3aea38c36c6adc7501d719c43621b6742f17ee57
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-18 23:20:57 +0300

    marya

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
         'method'  => 'POST',
-        'content' => http_build_query($data)
+        'content' => urlencode($data)
     )
 );
 $context  = stream_context_create($options);
