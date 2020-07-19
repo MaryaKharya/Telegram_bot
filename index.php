a92e1ac3f64ede56386952b631db26766e1aeecb
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-19 23:53:32 +0300

    marya'

--- index.php
+++ index.php
@@ -146,7 +146,7 @@ if (!empty($data['message']['text'])) {
             'sendMessage', 
             array(
                 'chat_id' => $data['message']['chat']['id'],
-                'text' => $ugu['data']['output']['url']
+                'text' => $out
             )
         );
         exit(); 
