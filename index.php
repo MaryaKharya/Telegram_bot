2291c56c5a6658ebd0ab8c603c4b8fa12d733fc4
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-20 19:30:01 +0300

    marya

--- index.php
+++ index.php
@@ -172,27 +172,14 @@ if (!empty($data['message']['text'])) {
         $result = $connection->query($id)->fetch();
         $convert = "SELECT con_id FROM conid WHERE user_chat_id = {$result['id']} ORDER BY id DESC LIMIT 1";
         $con = $connection->query($convert)->fetch();
-        sleep(10);
-        //get запрос на ссылку с конвертированным файлом
-		while (1)
-		{
 		$s = 'https://api.convertio.co/convert/' . $con['con_id'] . '/dl';
         $out = file_get_contents($s);
         $ugu = json_decode($out, true);
-		if (isset($ugu['data']['content']))
-		{
 		    sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
                                           'text' => $ugu['data']['content']
                                     )
                     );
-            break;					
-		}
-		else
-		{
-			sleep(3);
-		}
 
-		}
 		
         
         exit(); 
