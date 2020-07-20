74be9022c88f9274308ebc36eb6cd22b54975219
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-20 18:47:21 +0300

    marya

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ require 'vendor/autoload.php';
 $data = file_get_contents('php://input');
 $data = json_decode($data, true);
 
+define('TOKEN', '794519976:AAFVA4NguNYVsSymwPqn0iVHrBVoDIeMNnE');
+
 function databaseConnection(): PDO
 {
     static $connection = null;
@@ -25,7 +27,6 @@ if (empty($data['message']['chat']['id'])) {
     exit();
 }
  
-define('TOKEN', '794519976:AAFVA4NguNYVsSymwPqn0iVHrBVoDIeMNnE');
 
 // Функция вызова методов API.
 function sendTelegram($method, $response)
@@ -37,16 +38,16 @@ function sendTelegram($method, $response)
     curl_setopt($ch, CURLOPT_HEADER, false);
     $res = curl_exec($ch);
     curl_close($ch);
- 
+
     return $res;
 }
 
 if (!empty($data['message']['text'])) {
-	$text = $data['message']['text'];
-if ($text == '/start')
-{
-	    $connection = databaseConnection();
-		$sql = "INSERT INTO users (name, chat_id) VALUES ('{$data['message']['from']['first_name']}', '{$data['message']['chat']['id']}')";
+    $text = $data['message']['text'];
+    if ($text == '/start')
+    {
+        $connection = databaseConnection();
+        $sql = "INSERT INTO users (name, chat_id) VALUES ('{$data['message']['from']['first_name']}', '{$data['message']['chat']['id']}')";
         $connection->query($sql);
         sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
                                           'text' => 'Добро пожаловать! Я сконверирую все, что захочешь. Для этого выбири формат, который хочешь получить в результате конвертирования.
@@ -60,41 +61,38 @@ pdf           epub
 fb2           mobi'
                                          )
                     );
-    exit();
-}
+        exit();
+    }
 }
 
 if (!empty($data['message']['text'])) {
-	$text = $data['message']['text'];
-if ($text == 'jpg' || $text == 'jpeg' || $text == 'png' || $text == 'psd' || $text == 'gif' || $text == 'bmp' || $text == 'doc' || $text == 'docx' || $text == 'pdf' || $text == 'epub' || $text == 'fb2' || $text == 'mobi')
-{
-	    $connection = databaseConnection();
+    $text = $data['message']['text'];
+    if ($text == 'jpg' || $text == 'jpeg' || $text == 'png' || $text == 'psd' || $text == 'gif' || $text == 'bmp' || $text == 'doc' || $text == 'docx' || $text == 'pdf' || $text == 'epub' || $text == 'fb2' || $text == 'mobi')
+    {
+        $connection = databaseConnection();
         $id = "SELECT id FROM users WHERE chat_id = {$data['message']['chat']['id']}";
         $result = $connection->query($id)->fetch();
-		$sql = "INSERT INTO formats (format, user_id) VALUES ('{$text}', '{$result['id']}')";
+        $sql = "INSERT INTO formats (format, user_id) VALUES ('{$text}', '{$result['id']}')";
         $connection->query($sql);
         sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
                                           'text' => 'Cкинь фотографию или документ, который хочешь конвертировать'
                                          )
                     );
-    exit();
-}
+        exit();
+    }
 }
 
 // Прислали фото.
 if (!empty($data['message']['photo'])) {
     $photo = array_pop($data['message']['photo']);
-    $res = sendTelegram(
-        'getFile', 
-        array(
-            'file_id' => $photo['file_id']
-        )
-    );
+    $res = sendTelegram('getFile', array('file_id' => $photo['file_id']
+                                   )
+           );
     $res = json_decode($res, true);
     if ($res['ok']) {
         $src = 'https://api.telegram.org/file/bot' . TOKEN . '/' . $res['result']['file_path'];
         // отправка post запроса для получения id
-		$connection = databaseConnection();
+        $connection = databaseConnection();
         $id = "SELECT id FROM users WHERE chat_id = {$data['message']['chat']['id']}";
         $resul = $connection->query($id)->fetch();
         $format = "SELECT format FROM formats WHERE user_id = {$resul['id']} ORDER BY id DESC LIMIT 1";
@@ -115,32 +113,26 @@ if (!empty($data['message']['photo'])) {
         //Добавление id в базу данных.
         $sql = "INSERT INTO conid (con_id, user_chat_id) VALUES ('{$u['data']['id']}', '{$resul['id']}')";
         if ($connection->query($sql)) { 
-        //клавиатура
-        sendTelegram(
-            'sendMessage', 
-            array(
-                'chat_id' => $data['message']['chat']['id'],
-                'text' => 'результат придет в виде ссылки, ок?'
-            )
-        );
-    }
+            //клавиатура
+            sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
+                                              'text' => 'результат придет в виде ссылки, ок?'
+                                        )
+                        );
+        }
     }
     exit(); 
 }
  
 //отправление файла
 if (!empty($data['message']['document'])) {
-    $res = sendTelegram(
-        'getFile', 
-        array(
-            'file_id' => $data['message']['document']['file_id']
-        )
-    );
+    $res = sendTelegram('getFile', array('file_id' => $data['message']['document']['file_id']
+                                   )
+                       );
     $res = json_decode($res, true);
     if ($res['ok']) {
         $src = 'https://api.telegram.org/file/bot' . TOKEN . '/' . $res['result']['file_path'];
         // отправка post запроса для получения id
-		$connection = databaseConnection();
+        $connection = databaseConnection();
         $id = "SELECT id FROM users WHERE chat_id = {$data['message']['chat']['id']}";
         $resul = $connection->query($id)->fetch();
         $format = "SELECT format FROM formats WHERE user_id = {$resul['id']} ORDER BY id DESC LIMIT 1";
@@ -161,24 +153,21 @@ if (!empty($data['message']['document'])) {
         //Добавление id в базу данных.
         $sql = "INSERT INTO conid (con_id, user_chat_id) VALUES ('{$u['data']['id']}', '{$resul['id']}')";
         if ($connection->query($sql)) { 
-        //клавиатура
-        sendTelegram(
-            'sendMessage', 
-            array(
-                'chat_id' => $data['message']['chat']['id'],
-                'text' => 'результат придет в виде ссылки, ок?'
-            )
-        );
-    }
+            //клавиатура
+            sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
+                                              'text' => 'результат придет в виде ссылки, ок?'
+                                             )
+                        );
+        }
     }
     exit(); 
 }
 
 if (!empty($data['message']['text'])) {
-	$text = $data['message']['text'];
+    $text = $data['message']['text'];
     if ($text == 'ок') {
         //получение id из базы данных
-		$connection = databaseConnection();
+        $connection = databaseConnection();
         $id = "SELECT id FROM users WHERE chat_id = {$data['message']['chat']['id']}";
         $result = $connection->query($id)->fetch();
         $convert = "SELECT con_id FROM conid WHERE user_chat_id = {$result['id']} ORDER BY id DESC LIMIT 1";
@@ -192,13 +181,10 @@ if (!empty($data['message']['text'])) {
         $out = curl_exec($curl);
         curl_close($curl);
         $ugu = json_decode($out, true);
-        sendTelegram(
-            'sendMessage', 
-            array(
-                'chat_id' => $data['message']['chat']['id'],
-                'text' => $ugu['data']['output']['url']
-			)
-        );
+        sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
+                                          'text' => $ugu['data']['output']['url']
+                                    )
+                    );
         exit(); 
     } 
 }
