515033ff5a1ced854213ea3d297c8ff3d1386984
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-20 19:44:10 +0300

    marya

--- index.php
+++ index.php
@@ -172,7 +172,6 @@ if (!empty($data['message']['text'])) {
         $result = $connection->query($id)->fetch();
         $convert = "SELECT con_id FROM conid WHERE user_chat_id = {$result['id']} ORDER BY id DESC LIMIT 1";
         $con = $connection->query($convert)->fetch();
-        sleep(10);
         //get запрос на ссылку с конвертированным файлом
 		while (1)
 		{
