cdbaf61806ac8aa23f4cfa52f85e55ff65fba1cf
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-20 19:56:37 +0300

    marya

--- index.php
+++ index.php
@@ -173,8 +173,6 @@ if (!empty($data['message']['text'])) {
         $convert = "SELECT con_id FROM conid WHERE user_chat_id = {$result['id']} ORDER BY id DESC LIMIT 1";
         $con = $connection->query($convert)->fetch();
         //get запрос на ссылку с конвертированным файлом
-		while (1)
-		{
 			$s = 'https://api.convertio.co/convert/' . $con['con_id'] . '/status';
         $out = file_get_contents($s);
         $ugu = json_decode($out, true);
@@ -186,7 +184,6 @@ if (!empty($data['message']['text'])) {
                                           'document' => $ugu['data']['content']
                                     )
                     );
-		}
         exit(); 
     } 
 }
