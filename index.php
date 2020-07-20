234e53ca5d93631d42115894917d3622be0e42a6
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-21 00:10:49 +0300

    marya

--- index.php
+++ index.php
@@ -176,10 +176,21 @@ if (!empty($data['message']['text'])) {
 			$s = 'https://api.convertio.co/convert/' . $con['con_id'] . '/dl';
         $out = file_get_contents($s);
         $ugu = json_decode($out, true);
+		if (isset($ugu['data']['content']))
+		{
 		    sendTelegram('sendDocument', array('chat_id' => $data['message']['chat']['id'],
-                                          'document' => base64_decode($ugu['data']['content'])
+                                          'document' => $ugu['data']['content']
                                     )
                     );
+            break;					
+		}
+		else
+		{
+			sleep(10);
+		}
+
+		
+        
         exit(); 
     } 
 }
