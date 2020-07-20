b6c95f2b43de5eebee1a48c9081ad32075970449
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-20 19:06:17 +0300

    marya

--- index.php
+++ index.php
@@ -186,7 +186,7 @@ if (!empty($data['message']['text'])) {
 		if (isset($ugu['data']['output']['url']))
 		{
 		    sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
-                                          'text' => $ugu['data']['output']['url']
+                                          'text' => var_export($ugu, true) //['data']['output']['url']
                                     )
                     );
             break;					
