ded0bb556bd9203e98ebfc2d6ff3bd9fbbb6b610
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-21 16:39:21 +0300

    marya'

--- index.php
+++ index.php
@@ -16,9 +16,10 @@ $o = ('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAA+gAAAIyCAIAAADXE3dOAAAABGd
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace('', '+', $img);
 	$dama = base64_decode($img);
-	$file = tempnam('/tmp', 'FOO');
+	$file = fopen('php://memory', 'w+');
 	fwrite($file, $dama);
-    echo $file;
+	$success = stream_get_contents($file);
+    echo $success;
 
 function databaseConnection(): PDO
 {
