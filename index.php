429906c6371542c9b01eb29e8de1de0961c6912b
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-21 17:17:02 +0300

    marya'

--- index.php
+++ index.php
@@ -51,6 +51,9 @@ function sendTelegram($method, $response)
     return $res;
 }
 
+sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
+                                          'text' => 'Добро пожаловать! Я сконверирую все, что захочешь. Для этого выбири формат, который хочешь получить в результате конвертирования.
+
 if (!empty($data['message']['text'])) {
     $text = $data['message']['text'];
     if ($text == '/start')
