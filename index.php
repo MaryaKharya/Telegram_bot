98c4ef8ed23cf6f70c589ff08d73d277c8873053
MaryaKharya <korcemkinamaria76@gmail.com>
2020-07-21 08:06:11 +0300

    marya

--- index.php
+++ index.php
@@ -180,9 +180,10 @@ if (!empty($data['message']['text'])) {
         $ugu = json_decode($out, true);
 		if (isset($ugu['data']['content']))
 		{
-			$base64 = 'data:image/' . $result['id'] . ';base64,' . $ugu['data']['content'];
-		    sendTelegram('sendMessage', array('chat_id' => $data['message']['chat']['id'],
-                                          'text' => $o
+			$o = stripslashes($ugu['data']['content'])
+			$base64 = 'data:image/png;base64,' . $o;
+		    sendTelegram('sendPhoto', array('chat_id' => $data['message']['chat']['id'],
+                                          'photo' => $base64
                                     )
                     );
 		}
